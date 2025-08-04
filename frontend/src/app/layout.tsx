050ad98c7924daf2ddafb627d6aec3f7bff0497c
Joaco.M <joaco.martinez1480@gmail.com>
2025-08-04 01:00:09 -0300

    aaaa

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from '@vercel/analytics/next';
 import Navbar from "../components/navbar/Navbar";
 // import WhatsappLink from "@/components/WhatsappLink/WhatsappLink";
 import { AuthProvider } from '../context/authContext';
@@ -47,6 +48,7 @@ export default function RootLayout({
         </CheckoutProvider>
       </AnonCartProvider>
     </AuthProvider>
+    <Analytics />
   </body>
 </html>
   );
